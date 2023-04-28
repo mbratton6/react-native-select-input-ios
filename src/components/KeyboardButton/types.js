8c25dd0b59849a367fa32ac9350b5cedeff5aa58
mbratton6 <120400984+mbratton6@users.noreply.github.com>
2023-04-28 13:31:46 -0400

    Update types.js

--- src/components/KeyboardButton/types.js
+++ src/components/KeyboardButton/types.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types'
 import { Text } from 'react-native'
+import { TextPropTypes} from 'deprecated-react-native-prop-types'
+
 
 export default {
   onPress: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
   textAlign: PropTypes.string.isRequired,
   textStyle: PropTypes.oneOfType([
-    Text.propTypes.style,
-    PropTypes.arrayOf(Text.propTypes.style)
+    TextPropTypes.style,
+    PropTypes.arrayOf(TextPropTypes.style)
   ])
 }
