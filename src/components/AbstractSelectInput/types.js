91c5c7726a1afa1a01400552a681c8f7112a4d07
mbratton6 <120400984+mbratton6@users.noreply.github.com>
2023-04-28 13:30:45 -0400

    Update types.js

--- src/components/AbstractSelectInput/types.js
+++ src/components/AbstractSelectInput/types.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types'
 import { Text } from 'react-native'
+import { TextPropTypes} from 'deprecated-react-native-prop-types'
+
 
 export default {
   enabled: PropTypes.bool,
   labelStyle: PropTypes.oneOfType([
-    Text.propTypes.style,
-    PropTypes.arrayOf(Text.propTypes.style)
+    TextPropTypes.style,
+    PropTypes.arrayOf(TextPropTypes.style)
   ]),
   onEndEditing: PropTypes.func,
   onSubmitEditing: PropTypes.func,
